49b9811c5c7ddb2267f3467deaec093fc1eebf9c
Martin Chiquillo <martinschiquilloc@hotmail.com>
2022-05-17 23:06:08 -0500

    feat(API): Feature to get all workouts created

---
+++ src/controllers/workoutController.js
@@ -0,0 +1,36 @@
+//These methods will handle each endpoint route
+
+const workoutService = require('../services/workoutService');
+
+const getAllWorkouts = (req,res)=>{
+  const allWorkouts = workoutService.getAllWorkouts();
+  res.send({status:'OK',data:allWorkouts});
+};
+
+const getOneWorkout = (req,res)=>{
+  const workout = workoutService.getOneWorkout();
+  res.send('Get an existing workout');
+};
+
+const createNewWorkout = (req,res)=>{
+  const createdWorkout = workoutService.createNewWorkout();
+  res.send('Create a new workout');
+};
+
+const updateOneWorkout = (req,res)=>{
+  const updateWorkout = workoutService.updateOneWorkout();
+  res.send('Update an existing workout');
+};
+
+const deleteOneWorkout = (req,res)=>{
+  const deleteWorkout = workoutService.deleteOneWorkout();
+  res.send('Delete an existing workout');
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout
+};
