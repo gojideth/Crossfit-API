9c9b6034565929dc61409066c158461535002279
Martin Chiquillo <martinschiquilloc@hotmail.com>
2022-05-21 20:51:29 -0500

    fix(GET): Fixed undefined value when attempting to get a workout by id

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,36 +1,43 @@
-const DB = require('./db.json');
-const {saveToDatabase} = require('./utils');
+const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
-
-const getAllWorkouts = ()=>{
+const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-const getWorkoutById = (id)=>{
-  const workout = DB.workouts.find((workout)=>{
-    workout.id === id
-  });
-  if(!workout){
-    return;
-  }
-  console.log(workout);
-  return workout;
-}
-const createNewWorkout = (entryWorkout)=>{
-  const isCreatedAlready = DB.workouts.findIndex(
-    (workout) =>{
-      workout.name === entryWorkout.name
+const getWorkoutById = (id) => {
+  const workout = DB.workouts.find((workout) => {
+    if (workout.id === id) {
+      return workout;
+    } else {
+      return;
     }
-  )>-1;
-  if(isCreatedAlready){
+  });
+  return workout;
+};
+const createNewWorkout = (entryWorkout) => {
+  const isCreatedAlready =
+    DB.workouts.findIndex((workout) => {
+      workout.name === entryWorkout.name;
+    }) > -1;
+  if (isCreatedAlready) {
     return;
   }
   DB.workouts.push(entryWorkout);
   saveToDatabase(DB);
   return entryWorkout;
+};
 
-}
+/**DB.workouts.forEach((workout)=>{
+  console.log(workout.id + "--" + typeof workout.name + "---" + workout.name); 
+});
+const test = DB.workouts.find((workout) => {
+  if (workout.name === "Perreo 2 Testeo") {
+    return workout;
+  } else {
+    return;
+  }
+});
+console.log("---> " + test.name);*/
 
-
-
-module.exports = {getAllWorkouts, createNewWorkout, getWorkoutById};
+module.exports = { getAllWorkouts, createNewWorkout, getWorkoutById };
